b4e48a55fb5fa67016bd53bccb9a03e5768003f2
eugene <uuzh68@gmail,com>
2017-07-05 15:59:36 +0300

    up

--- js/sw.js
+++ js/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'my-site-cache-v1';
+var CACHE_NAME = 'uuzh68-site-cache-v1';
 var urlsToCache = [
     '/',
     '/css/bootstrap.min.css',
@@ -15,5 +15,19 @@ self.addEventListener('install', function(event) {
                 console.log('Opened cache');
                 return cache.addAll(urlsToCache);
             })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                    // Cache hit - return response
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                }
+            )
     );
 });
